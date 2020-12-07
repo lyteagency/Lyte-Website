3920ac8cab95506c6e9d5a18674cc3da2cd36e75
Steven Porter <steven.porter@guddesign.co.uk>
2020-12-07 00:39:03 +0000

    upload initial draft of website

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,17 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+let formElements = document.querySelectorAll('input, textarea');
+
+const checkIfEmpty = function (event) {
+    if (event.target.value !== '')
+    {
+        event.target.classList.remove('empty')
+    }
+}
+formElements.forEach(function(el) {
+    el.addEventListener('load', checkIfEmpty, false);
+    el.addEventListener('focus', checkIfEmpty, false);
+    el.addEventListener('keyup', checkIfEmpty, false);
+});
